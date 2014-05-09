6da87bab68074e23739dcd8f9255dd29c0cdfab8
Eugen Waldschmidt <info@zeotyn.de>
2014-05-10 01:47:57 +0200

    added the full app, after adding new method to define the device and to get the right url

---
+++ GoCiOS/GoCiOS/gocAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gocAppDelegate.h
+//  GoCiOS
+//
+//  Created by Eugen Waldschmidt on 16.04.14.
+//  Copyright (c) 2014 Zeotyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gocAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
